ed1b55072f40a6fd59f6db5135ca9e8a1908f878
Jacob <jelondon12@gmail.com>
2019-10-17 10:44:57 -0700

    uncommented parse from testing

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 int main()
 {
     Processor p(1000);
-    //Parser parse("ex.s");
-    //std::tie(p.mem.imem, std::ignore) = parse.parse();
+    Parser parse("ex.s");
+    std::tie(p.mem.imem, std::ignore) = parse.parse();
 
     for (int i = 0; i < 50; i++)
         p.mem.insert(Instruction(NOP, false, AL, ZERO, ZERO, ZERO, 0, NOF));
